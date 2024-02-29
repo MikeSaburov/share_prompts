1ffed904482a4762b7737b9df453b1553d8dd865
Mikhail Saburov <saburov1985@gmail.com>
2024-02-29 22:22:42 +1100

    feature(error): добавлен файл ошибок

--- app/posts/layout.js
+++ app/posts/layout.js
@@ -1,10 +1,13 @@
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 import React from 'react';
-
+import Error from './error';
 const layout = ({ children }) => {
   return (
     <div>
-      {children}
-      <p>Вернуться наверх</p>
+      <ErrorBoundary fallback={<Error />}>
+        {children}
+        <p>Вернуться наверх</p>
+      </ErrorBoundary>
     </div>
   );
 };
