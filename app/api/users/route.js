9bd5cd124449beb302c1f58f2c084eb553f6824b
Mikhail Saburov <saburov1985@gmail.com>
2024-03-02 22:13:55 +1100

    refactor(app): очистил проект
    НАЧИНАЮ РАЗРАБОТКУ

--- app/api/users/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET(req) {
-  const users = [
-    { id: 1, name: 'Mike' },
-    { id: 2, name: 'Sergey' },
-    { id: 3, name: 'Valya' },
-  ];
-
-  return new Response(JSON.stringify(users));
-}
--- app/posts/[postId]/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const page = ({ params }) => {
-  return <div>Пост № {params.postId}</div>;
-};
-
-export default page;
