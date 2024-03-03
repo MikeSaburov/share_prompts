fdd0307f3d583440657f22f2a834c2188e823d83
Mikhail Saburov <saburov1985@gmail.com>
2024-03-03 22:48:28 +1100

    добавил логотип

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,12 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg" />
+        <Image
+          src="/assets/images/logo.svg"
+          width={30}
+          height={30}
+          alt="Logo"
+        />
       </Link>
     </nav>
   );
