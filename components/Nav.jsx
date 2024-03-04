ee2dc372cdac208c3786c3f3060f9b6a6e336785
Mikhail Saburov <saburov1985@gmail.com>
2024-03-04 21:50:23 +1100

    Добавил кнопку вый

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -90,6 +90,23 @@ const Nav = () => {
                 >
                   My Profile
                 </Link>
+                <Link
+                  href={'/create-prompt'}
+                  className="dropdown_link"
+                  onClick={() => setToglleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToglleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
