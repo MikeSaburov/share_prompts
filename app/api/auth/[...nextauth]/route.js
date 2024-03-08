b9ef7e0e69237dfcb7b837b383c8498fe63e38b4
Mikhail Saburov <saburov1985@gmail.com>
2024-03-08 22:29:48 +1100

    реализована функция входа в систему

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
+
 import { connectToBD } from '@utils/database';
 
 const handler = NextAuth({
@@ -14,8 +17,17 @@ const handler = NextAuth({
     try {
       await connectToBD();
       // 1. Проверить существует ли пользватель
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
       // 2. Если нет пользователя, создать его и сохранить в БД
-
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
       return true;
     } catch (error) {
       console.log('Не удалось подключиться к БД', error);
