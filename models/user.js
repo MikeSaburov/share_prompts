f6ec1df09132909b1982d468b9820ac2617fbf0b
Mikhail Saburov <saburov1985@gmail.com>
2024-03-08 22:19:47 +1100

    написал схему пользователя

--- models/user.js
+++ models/user.js
@@ -1,1 +1,21 @@
 import { Schema, models, model } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    require: [true, 'Введите кооректный email'],
+  },
+  username: {
+    type: String,
+    unique: [true, 'Имя уже занято, пожалуйста придумайте другое!'],
+    require: [true, 'Введите имя пользователя!'],
+    match: [
+      /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      'Неверное имя, имя должно содержать от 8 до 20 символов и должно быть уникальным!',
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
