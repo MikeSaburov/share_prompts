18481d07c500491d776f7470a2db2ddc2037e78c
Mikhail Saburov <saburov1985@gmail.com>
2024-03-08 21:02:29 +1100

    написал логику подключения к БД

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,24 @@
 import mongoose from 'mongoose';
 
 let isConnected = false; //отслеживает состояние соединения к БД
+
+// Подключение к БД
+export const connectToBD = async () => {
+  // Строгий запрс к БД
+  mongoose.set('strictQuery', true);
+  if (isConnected) {
+    console.log('MongoDB alrady is connect');
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      dbName: 'share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('DB connect');
+  } catch (error) {
+    console.log(error);
+  }
+};
