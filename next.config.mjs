c71275356afcffdf45bd1b94c0ec68dc84b3d47a
Mikhail Saburov <saburov1985@gmail.com>
2024-03-20 22:29:35 +1100

    рефакторинг

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ['mongoose'],
-  },
+  // experimental: {
+  //   appDir: true,
+  //   serverComponentsExternalPackages: ['mongoose'],
+  // },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+    // domains: [''],
   },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
+  // webpack(config) {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     topLevelAwait: true,
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
