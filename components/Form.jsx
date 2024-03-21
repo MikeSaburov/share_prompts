4a2bd3fa20f1b0f164930caa1dd9e84763d05fe6
Mikhail Saburov <saburov1985@gmail.com>
2024-03-21 22:39:58 +1100

    верстка компоненты Form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>
-      <h1>DKDKDK</h1>
-    </div>
+    <section className="w-full max-w-full flex flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing with the world? and let your imagination run
+        wild with any AI-powered platform.
+      </p>
+    </section>
   );
 };
 
