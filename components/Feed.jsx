3baaa8c6ed65d6e03264e08d98f93e39dd993a08
Mikhail Saburov <saburov1985@gmail.com>
2024-04-01 22:14:12 +1100

    feature(Feed):вёрстка

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,35 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  <div className="mt-16 prompt_layout">fdf</div>;
+};
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          className="search_input peer"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
