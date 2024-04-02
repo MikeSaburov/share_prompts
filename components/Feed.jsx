2e214a651e48b118d19911bd3b3ab0cec34e6ec6
Mikhail Saburov <saburov1985@gmail.com>
2024-04-02 21:12:18 +1100

    refactor(Feed): реализовано получение всех постов на главной

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,15 +5,30 @@ import { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  <div className="mt-16 prompt_layout">fdf</div>;
+  <div className="mt-16 prompt_layout">
+    {data.map((post) => (
+      <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+    ))}
+  </div>;
 };
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
     setSearchText(e.target.value);
   };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <section className="feed">
@@ -27,7 +42,7 @@ const Feed = () => {
           required
         />
       </form>
-      <PromptCardList data={[]} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
