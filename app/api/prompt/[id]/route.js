e0b8079f9a6a53451fd125a5988cdeea3aede965
Mikhail Saburov <saburov1985@gmail.com>
2024-04-10 20:21:21 +1100

    feature(api/prompt/[id]): реализован метод обновления подсказки

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,7 +2,6 @@ import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
 //GET (read)
-
 export const GET = async (req, { params }) => {
   try {
     await connectToDB();
@@ -24,5 +23,31 @@ export const GET = async (req, { params }) => {
 };
 
 //PATCH (update)
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt) {
+      return new Response(JSON.stringify('Подсказка не найдена'), {
+        status: 404,
+      });
+    }
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify('Не удалось обновить подсказку😢'), {
+      status: 500,
+    });
+  }
+};
 
 //DELETE (delete)
