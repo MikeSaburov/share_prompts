24b5a5444ed8b1465d4482b77ad2b3fa525f2ed4
Mikhail Saburov <saburov1985@gmail.com>
2024-04-11 20:44:54 +1100

    feature(profile): реализована возможность удаления поста

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,7 +26,21 @@ const MyProfile = () => {
   const handleEdit = async (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this post?');
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filterPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filterPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
